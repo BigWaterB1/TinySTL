fb426769c74b7c094f591b51021bb3736c6e28b4
BigWaterB1 <806405847@qq.com>
2025-04-26 00:31:28 +0800

    init

---
+++ readme.md
@@ -0,0 +1,9 @@
+# TinySTL
+1. vector
+2. rb tree
+3. hash table
+4. set
+5. unordered_set
+6. map
+7. unordered_map
+
---
+++ vector.h
@@ -0,0 +1,13 @@
+#ifndef TINYSTL_VECTOR
+#define TINYSTL_VECTOR
+
+namespace tinySTL {
+
+class vector {
+    
+};
+
+
+}
+
+#endif // TINYSTL_VECTOR
